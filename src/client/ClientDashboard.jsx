753eb10fc47f5e2443a986508cd02db1ccb21872
data4decision international <data4decision25@gmail.com>
2025-07-19 09:33:15 -0700

    Update: Improved ClientDashboard layout and logic

--- src/client/ClientDashboard.jsx
+++ src/client/ClientDashboard.jsx
@@ -1,32 +1,12 @@
-// import React from 'react'
-// import BeninDashboard from './countries/BeninDashboard'
-// import NigeriaDashboard from './countries/NigeriaDashboard'
 
-import CompareView from "../components/CompareView"
-import CountryComparisonChart from "../components/CountryComparisonChart"
 import DashboardWelcomeSection from "../components/DashboardWelcomeSection"
-
-// const ClientDashboard = () => {
-//   return (
-//     <div><BeninDashboard/>
-//     <NigeriaDashboard/>
-//     </div>
-//   )
-// }
-
-// export default ClientDashboard
-
-
-
-
 
 
 const ClientDashboard = () => {
   return (
     <div>
     <DashboardWelcomeSection/>
-    <CountryComparisonChart/>
-    <CompareView/>
+    
     </div>
   )
 }
